7df9a365e65cdee37a0ea359127d4fac8b2d75fd
karma <166899236+projectnirvana@users.noreply.github.com>
2025-09-11 16:56:10 +0530

    Update start.py

--- Toxic/plugins/bot/start.py
+++ Toxic/plugins/bot/start.py
@@ -84,8 +84,8 @@ async def start_pm(client, message: Message, _):
                 )
     else:
         out = private_panel(_)
-        await message.reply_photo(
-            photo=config.START_IMG_URL,
+        await message.reply_video(
+            video=config.START_VID_URL,
             caption=_["start_2"].format(message.from_user.mention, app.mention),
             reply_markup=InlineKeyboardMarkup(out),
         )
@@ -150,4 +150,5 @@ async def welcome(client, message: Message):
                 await message.stop_propagation()
         except Exception as ex:
             print(ex)
+
 
